24ff78d8a16b441f398f2c0d4d244da101c09228
dangduongcoder <truongdangduong666@gmail.com>
2021-09-09 22:05:20 +0700

    first upload

---
+++ public/js/CONFIG.js
@@ -0,0 +1,58 @@
+const __DATA__ = {
+    'config' : {
+        'title' : 'Các liên kết của tôi',
+        'favicon' : 'avatar.jpg',
+    },
+    'cover-image' : 'Mt.FujiJapan-day.gif',
+    'avatar-image' : 'avatar2.jpg',
+    'name' : 'Trương Đăng Dương', 
+    'skills' : [
+        'Developer',
+        'Designer',
+    ],
+    'bio-storys' : [
+        {
+            'image' : 'avatar.jpg',
+            'content' : 'Tiền, tiền, tiền...',
+            'author' : 'Trương Đăng Dương',
+        },
+    ],
+    'links' : [
+        {
+            'image' : 'facebook.jpg',
+            'title' : 'Facebook', 
+            'show-value' : '@duongdzin',
+            'value' : 'https://www.facebook.com/duongdzin/',
+            'type' : 0,
+        },
+        {
+            'image' : 'instagram.jpg',
+            'title' : 'Instagram', 
+            'show-value' : '@dark_x_d',
+            'value' : 'https://www.tiktok.com/',
+            'type' : 0,
+        },
+        {
+            'image' : 'tiktok.jpg',
+            'title' : 'Tiktok', 
+            'show-value' : '@tiktok_official',
+            'value' : 'https://www.instagram.com/dark_x_d/',
+            'type' : 0,
+        },
+        {
+            'image' : 'mbbank.jpg',
+            'title' : 'MBBank', 
+            'show-value' : 'TRUONG DANG DUONG - 0369895206',
+            'value' : '0369895206',
+            'type' : 1,
+        },
+        {
+            'image' : 'momo.jpg',
+            'title' : 'MOMO', 
+            'show-value' : '0369895206',
+            'value' : '0369895206',
+            'type' : 1,
+        },
+    ]
+};
+
